d7d60dea181929a978b024c63b365086e694e5b7
anisafirdi <anisa.fir2002@mail.ugm.ac.id>
2023-11-24 00:11:25 +0700

    first commit

---
+++ maps/output.js
@@ -0,0 +1,165 @@
+const data = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RSUP DR. SARDJITO",
+        ALAMAT: "Jl. Kesehatan No.1, Senolowo, Sinduadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55281",
+        TELP: "(0274) 631190"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.3734475283894,
+          -7.767935794555573,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RSUD SLEMAN",
+        ALAMAT: "Jl. Bhayangkara No.48, Temulawak, Triharjo, Kec. Sleman, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55514",
+        TELP: "(0274) 868437"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.3418978995528,
+          -7.686878701628561,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RSUD PRAMBANAN",
+        ALAMAT: "Delegan, Jl. Raya Piyungan - Prambanan No.KM. 7, Dinginan, Sumberharjo, Kec. Prambanan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55572",
+        TELP: "(0274) 4398356"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.4831505835028,
+          -7.80463845103656,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RS JOGJAKARTA INTERNATIONAL HOSPITAL",
+        ALAMAT: "Jl. Ring Road Utara No.160, Perumnas Condong Catur, Condongcatur, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55283",
+        TELP: "(0274) 4463535"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.4029825942112,
+          -7.75799609936896,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RS PANTI RINI",
+        ALAMAT: "Jl. Raya Solo - Yogyakarta KM.13,2, Kringinan, Tirtomartani, Kec. Kalasan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55571",
+        TELP: "(0274) 496022"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.4667454306488,
+          -7.772157155803583,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RS SAKINA IDAMAN",
+        ALAMAT: "Condro Loekito, Jl. Nyi Tjondrolukito Jl. Blunyah Gede No.60, Kutu Dukuh, Sinduadi, Kec. Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55284",
+        TELP: "(0274) 8000111"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.3680977079765,
+          -7.767653378334843,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RS PKU MUHAMMADIYAH GAMPING",
+        ALAMAT: "Jl. Wates, Jl. Nasional III KM.5,5, Bodeh, Ambarketawang, Kec. Gamping, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55294",
+        TELP: "(0274) 6499704"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.3177105344376,
+          -7.800405811816248,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RS BHAYANGKARA",
+        ALAMAT: "Jl. Raya Solo - Yogyakarta KM.14, Glondong, Tirtomartani, Kec. Kalasan, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55571",
+        TELP: "(0274) 498278"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.4718233300217,
+          -7.766211982001137,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RS AKADEMIK UGM",
+        ALAMAT: "Jl. Kabupaten, Kranggahan I, Trihanggo, Kec. Gamping, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55291",
+        TELP: "(0274) 4530404"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.3503886258523,
+          -7.743158322327536,
+          0
+        ]
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        NAMA: "RS HERMINA",
+        ALAMAT: "Jl. Selokan Mataram, RT.06/RW.50, Meguwo, Maguwoharjo, Kec. Depok, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55282",
+        TELP: "(0274) 2800808"
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [
+          110.4327647236842,
+          -7.770251589592063,
+          0
+        ]
+      }
+    }
+  ]
+}
